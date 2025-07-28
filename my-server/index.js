4767504abf6aa1050085a740ec815d94dbb51e13
Tannu257 <tannutannu3849@gmail.com>
2025-07-28 14:36:51 +0530

    Update index.js

--- my-server/index.js
+++ my-server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-const PORT = 3001;
+const PORT = 'https://tannufrontend.onrender.com';
 
 // Middleware
 app.use(express.json());
